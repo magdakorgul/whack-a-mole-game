a8d7f445c6778fdad25c64026f85cbaa877836de
magdakorgul <magdalenakorgul1@gmail.com>
2024-04-19 15:23:48 +0200

    feature: generate ibstacles in random positions

--- js/main.js
+++ js/main.js
@@ -36,9 +36,8 @@ class Obstacle{
     constructor(){
         this.width = 10;
         this.height = 10;
-        this.positionX = 50 - this.width/2;
-        this.positionY = 80;
-        
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+        this.positionY = 100;
         this.obstacleElement = null;
 
         this.createDomElement();
