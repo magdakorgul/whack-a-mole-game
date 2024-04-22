c2e92c2f02301ca682a09222d64a7ac61f663d54
magdakorgul <magdalenakorgul1@gmail.com>
2024-04-22 16:18:49 +0200

    Added a grid, worked on the grid and placed an image on a square. Matched CSS selector to JS file.

--- js/main.js
+++ js/main.js
@@ -1,102 +1,45 @@
 
 
-class Player{
+class Board{
     constructor(){
-        this.width = 15;
-        this.height = 15;
-        this.positionX = 50 - this.width/2;
-        this.positionY = 0;
-        
+        this.width = 70;
+        this.height = 70;
+        this.positionX = 50;
+        this.positionY = 20;
 
-        this.playerElm = document.getElementById("player");
-        this.playerElm.style.width = this.width +"vw";
-        this.playerElm.style.height = this.height + "vh";
-        this.playerElm.style.bottom = this.positionY + "vh";
-        this.playerElm.style.left = this.positionX + "vw";
-        
+        this.square = document.querySelector("#board-game");
+        this.square.style.width = this.width + "vw";
+        this.square.style.height = this.height + "vh";
+        this.square.style.bottom = this.positionY + "vh";
+        this.square.style.left = this.positionX + "vw";
         
     }
-
-moveLeft(){
-    if(this.positionX > 0){
-    this.positionX--;
-    this.playerElm.style.left = this.positionX + "vw";
-    }
-
 }
-moveRight(){
-    if(this.positionX < 100 - this.width){
-    this.positionX++; // or: this.position += 1
-    this.playerElm.style.left = this.positionX + "vw";
-    }
-}
-}
+const boardGame = new Board();
 
-class Obstacle{
+class Square{
     constructor(){
-        this.width = 10;
-        this.height = 10;
-        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
-        this.positionY = 100;
-        this.obstacleElement = null;
+        this.width = 17;
+        this.height = 17;
+        this.positionX = 50;
+        this.positionY = 20;
 
-        this.createDomElement();
-    }
-    createDomElement(){
-        this.obstacleElement = document.createElement("div");
+        this.squareElement = document.querySelectorAll(".square");
 
-        this.obstacleElement.className = "obstacle";
-        this.obstacleElement.style.width = this.width + "vw"
-        this.obstacleElement.style.height = this.height + "vh";
-        this.obstacleElement.style.left = this.positionX + "vw";
-        this.obstacleElement.style.border = this.positionY + "vh";
-        
+        for (let i=0; i<this.squareElement.length; i++){
+            let element = this.squareElement[i];
+        element.style.width = this.width + "vw";
+        element.style.height = this.height + "vh";
+        element.style.bottom = this.positionY + "vh";
+        element.style.left = this.positionX + "vw";
 
-        const parentElement = document.getElementById("board");
-        parentElement.appendChild(this.obstacleElement);
-    }
+        let image = document.querySelector(".rat");
+        image.style.width = "80px";
+        image.style.maxHeight = "100%";
+        image.style.position = "relative";
 
-
-    moveDown(){
-    this.positionY--;
-    this.obstacleElement.style.bottom = this.positionY + "vw";
-     }
+         element.appendChild(image);
+    };
 }
-
-const player = new Player();
-
-const obstaclesArr = [];
-
-//const o1 = new Obstacle();
-
-setInterval(() => {
-    const newObstacle = new Obstacle();
-    obstaclesArr.push(newObstacle);
-}, 3000);
-
-
-//move obstacle
-setInterval(() => {
-    obstaclesArr.forEach((obstacleInstance) => {
-        obstacleInstance.moveDown();
-
-        if(
-            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
-            player.positionX + player.width > obstacleInstance.positionX &&
-            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
-            player.positionY + player.height > obstacleInstance.positionY
-        ){
-            console.log("game over");
-            location.href = "gameover.html";
-        }
-    });
-}, 80);
-
-document.addEventListener("keydown", (e) => {
-   if (e.code === "ArrowLeft"){;
-    player.moveLeft();
 }
-else if(e.code === "ArrowRight"){
-    player.moveRight();
-}
-});
+const houses = new Square();
